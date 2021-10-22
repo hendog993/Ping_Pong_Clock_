3d9f8cb4196b242de1ad04b0aaddb5c0b55d8fea
Henry Gilbert <henrygilbert609@gmail.com>
2021-10-22 10:37:23 -0500

    Initial commit

---
+++ Ping_Pong_Clock_.ino
@@ -0,0 +1,21 @@
+/* Write Time values  */
+    for ( int i = 0; i < 4 ; i++ ) 
+    {
+        for ( int j = 0 ; j<12 ; j++ ) 
+        {
+            if ( nums [ time_values[ i ]] [ j ] == 1 )
+            {
+                leds[digits[i][j]] = CRGB( 0, 0, 255 );
+            }
+        }
+    }
+asdfasdf
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
